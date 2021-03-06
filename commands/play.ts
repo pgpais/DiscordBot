fd7217efceb32e4713bbab51540fb8e1638f6cca
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-03-06 21:35:23 +0000

    stuffs

--- commands/play.ts
+++ commands/play.ts
@@ -3,14 +3,18 @@ import { IExecute } from "../interfaces/ICommands";
 
 export const name = "play";
 export const description = "play stuff";
-export const execute:IExecute = async (client, message, args) => {
+export const execute: IExecute = async (client, message, args) => {
   const voiceChannel = message.member.voice.channel;
 
   if (!voiceChannel)
     return message.channel.send("Join a voice channel you dweeb");
   const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!args.length)
-    return message.channel.send("Wrong number of args");
+  if (!args.length) return message.channel.send("Wrong number of args");
 
   const connection = await voiceChannel.join();
-}
+
+
+
+};
+
+

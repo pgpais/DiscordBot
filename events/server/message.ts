879c09eb85c82ae32f7672f623443cec6d3741d3
pgpais <pgpaisdev@gmail.com>
2021-03-06 19:53:30 +0000

    Message event implemented

--- events/server/message.ts
+++ events/server/message.ts
@@ -1,22 +1,19 @@
+import { Message } from "discord.js";
 import {IExecute} from "../../interfaces/IEvents";
 
-export const execute:IExecute = async (client, message) => {
-    
-}
+export const name:string = "message";
+export const execute:IExecute = async (client, message:Message) => {
+    if (!message.content.startsWith(client.prefix) || message.author.bot) return;
 
-// module.exports = (client, message) => {
-//   const prefix = process.env.DISCORD_PREFIX;
-//   if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(client.prefix.length).split(/ +/);
 
-//   const args = message.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
 
-//   const cmd = args.shift().toLowerCase();
+  const command = client.commands.get(cmd);
 
-//   const command = client.commands.get(cmd);
-
-//   if (command){
-//      command.execute(client, message, args, Discord)
-//   } else {
-//     message.channel.send('Unknown command! Speak to the developers if you think you found a bug!');
-//   };
-// };
+  if (command){
+     command.execute(client, message, args)
+  } else {
+    message.channel.send('Unknown command! Speak to the developers if you think you found a bug!');
+  };
+};

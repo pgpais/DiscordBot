cbcf2f821fefb301be47746e5d36be63aa136c3d
pgpais <pgpaisdev@gmail.com>
2021-03-07 17:10:05 +0000

    Wrong directory

--- commands/goodBot.ts
+++ commands/goodBot.ts
@@ -5,6 +5,6 @@ export const name = "goodbot";
 export const description = "Good bot";
 export const execute:IExecute = async (client, message, args) => {
     //message.channel.send('pong!');
-    const attachment = new MessageAttachment('../images/bot_icon.gif');
+    const attachment = new MessageAttachment('./images/bot_icon.gif');
     message.channel.send('', attachment);
 }

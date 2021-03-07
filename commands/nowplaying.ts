7b4f8e2120b480fc58eb6a0c507c4fa53f99013c
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-03-07 19:31:05 +0000

    np

--- commands/nowplaying.ts
+++ commands/nowplaying.ts
@@ -1,8 +1,20 @@
+import axios from "axios";
 import { IExecute } from "../interfaces/ICommands";
+import data from "../radios.json";
 
 export const name = "nowplaying";
 export const description = "get what is playing";
-export const execute:IExecute = async (client, message, args) => {
-    //message.channel.send('pong!');
-    
-}
+export const execute: IExecute = async (client, message, args) => {
+  //message.channel.send('pong!');
+  let radio = data[args.join(" ").toUpperCase()];
+  let radioId = radio.id;
+
+  const url: string = `https://prod.radio-api.net/stations/now-playing?stationIds=${radioId}`;
+
+  try {
+    const response = await axios.get(url);
+    message.channel.send(response.data[0].title);
+  } catch (exception) {
+    process.stderr.write(`ERROR received from ${url}: ${exception}\n`);
+  }
+};

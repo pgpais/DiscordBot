e1a62b68de487611f15ed058526b4eb31eb83fc2
pgpais <pgpaisdev@gmail.com>
2021-03-07 17:04:20 +0000

    goodbot

--- commands/goodBot.ts
+++ commands/goodBot.ts
@@ -1,8 +1,8 @@
 import { MessageAttachment } from "discord.js";
 import { IExecute } from "../interfaces/ICommands";
 
-export const name = "goodBot";
-export const description = "";
+export const name = "goodbot";
+export const description = "Good bot";
 export const execute:IExecute = async (client, message, args) => {
     //message.channel.send('pong!');
     const attachment = new MessageAttachment('../images/bot_icon.gif');
